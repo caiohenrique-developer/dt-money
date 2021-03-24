ad95266c87c416c76531fef221e03af7fad9b3bd
caiohenrique-developer <caiorussellrox@msn.com>
2021-03-24 15:57:15 -0300

    :hammer: [ CHORE ]: The initial project structure done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GlobalStyles } from "./styles/globals";
+
 export function App() {
   return (
     <>
       <div className="App">
         <h1>É isso ai!! ;)</h1>
+        <GlobalStyles />
       </div>
     </>
   );
---
+++ src/styles/globals.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+    /* Root variables */
+    :root {
+        --shape:#fff;
+        --background:#f0f2f5;
+        --text-body:#969cb3;
+        --text-title:#363f5f;
+        --blue-light:#6933ff;
+        --blue:#5429cc;
+        --red:#e52e4d;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: none;
+        
+        font-family: 'Poppins', sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    html {
+        @media(max-width: 1080px) { font-size: 93.75%; }
+
+        @media(max-width: 720px) { font-size: 87.5%; }
+     
+        body, input, textarea, button { font-weight: 400; }
+
+        body {
+            background: Var(--background);
+        
+            bottom { cursor: pointer; }
+
+            [disabled] {
+                opacity: 0.6;
+                cursor: not-allowed;
+            }
+
+            h1, h2, h3, h4, h5, h6, strong { font-weight: 600; }
+        }
+    }
+`
